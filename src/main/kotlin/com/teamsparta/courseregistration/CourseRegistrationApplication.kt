1b149442f3c398c8b58a5c5312318d0924b7d906
Alice <hifumialice@gmail.com>
2024-06-04 15:42:34 +0900

    AOP 사용하려고 Main에 등록

--- src/main/kotlin/com/teamsparta/courseregistration/CourseRegistrationApplication.kt
+++ src/main/kotlin/com/teamsparta/courseregistration/CourseRegistrationApplication.kt
@@ -2,9 +2,9 @@ package com.teamsparta.courseregistration
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 
-
-
+@EnableAspectJAutoProxy // 심화에서 추가
 @SpringBootApplication
 class CourseRegistrationApplication
 
